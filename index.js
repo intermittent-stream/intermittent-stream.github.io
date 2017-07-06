71e515e3efbb9aa98923ed1bc392d95897021bc9
Logan Williams <logan.williams@alum.mit.edu>
2017-07-06 14:02:15 -0700

    Added look up

---
+++ index.js
@@ -0,0 +1,31 @@
+/* global L */
+;(function (window) {
+  function init (mapid) {
+    var minZoom = 0
+    var maxZoom = 7
+    var img = [
+      32768, // original width of image `karta.jpg`
+      32768  // original height of image
+    ]
+
+    // create the map
+    var map = L.map(mapid, {
+      minZoom: minZoom,
+      maxZoom: maxZoom
+    })
+
+    // assign map and image dimensions
+    var rc = new L.RasterCoords(map, img)
+
+    // set the view on a marker ...
+    map.setView(rc.unproject([16384, 16384]), 2)
+
+    // the tile layer containing the image generated with gdal2tiles --leaflet ...
+    L.tileLayer('./tiles/{z}/{x}/{y}.jpg', {
+      noWrap: true,
+      attribution: 'Project by <a href="http://subject.space">Logan Williams</a>.'
+    }).addTo(map)
+  }
+  
+  init('map')
+}(window))
